1be6d91b4914961a1c33e41940b6a2c7c7d48b45
Eric Heinitz <ericheinitz@gmail.com>
2023-02-19 21:04:26 -0300

    fix class className

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -20,7 +20,7 @@ const AuthLayout = () => {
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost">
                             {user?.name}
-                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow-2x1 bg-base-300 rounded-box w-52">
                             <li>
